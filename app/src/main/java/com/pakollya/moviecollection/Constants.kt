afd133c2c5f6dc2c9fe8c6e0131fe447e360417f
nick <n1ck.bl4ck0ut@gmail.com>
2022-05-13 13:15:43 +0300

    #3 Добавить splash Исправить дизайн movie_item.xml и activity_main.xml Исправить иконку приложения

--- app/src/main/java/com/pakollya/moviecollection/Constants.kt
+++ app/src/main/java/com/pakollya/moviecollection/Constants.kt
@@ -5,3 +5,5 @@ const val BASE_URL = "https://api.nytimes.com/svc/movies/v2/"
 const val REQUEST_REVIEWS_ALL = "reviews/all.json"
 
 const val API_KEY = "HxwlK91PvftXATmFUJ4AwPxY2LW7v9pI"
+
+const val SPLASH_TIMER = 3000L
---
+++ app/src/main/java/com/pakollya/moviecollection/presentation/splash/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.pakollya.moviecollection.presentation.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.pakollya.moviecollection.R
+import com.pakollya.moviecollection.SPLASH_TIMER
+import com.pakollya.moviecollection.presentation.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIMER)
+    }
+}
